b569c6b23218bd8c964e5fbbbd528bc6918f18ab
jacklubbs <jack.lubbock@bitrise.io>
2024-04-15 22:29:49 +0100

    Initial Commit

---
+++ getgitworking/getgitworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  getgitworkingApp.swift
+//  getgitworking
+//
+//  Created by Jack Lubbock on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct getgitworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
