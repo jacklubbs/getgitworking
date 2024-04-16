a5b9b37b63c2917f8f96862f93975ec2ef6d133f
jacklubbs <jack.lubbock@bitrise.io>
2024-04-16 21:42:00 +0100

    pipeline_build

---
+++ getgitworkingUITests/getgitworkingUITests2.swift
@@ -0,0 +1,21 @@
+//
+//  getgitworkingUITests.swift
+//  getgitworkingUITests
+//
+//  Created by Jack Lubbock on 15/04/2024.
+//
+
+import XCTest
+@testable import getgitworking
+
+final class getgitworkingUITests2: XCTestCase {
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
